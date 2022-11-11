0f80b22725ea4d1af57d8b1f512ee2382058d539
Luma Montes <lumagoesmontes@gmail.com>
2022-11-11 06:10:08 +0000

    ajuste em cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.DEPLOY_URL);
+    res.header("Access-Control-Allow-Methods", "HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    res.header("Access-Control-Expose-Headers", "Content-Length, X-JSON");
+    app.use(cors());
+    return next();
+  });
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
